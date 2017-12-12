dacc2e5ed2bf2f86a7f569ffe648e2b2eef2df4d
Randall Pruim <rpruim@gmail.com>
2017-12-12 09:59:38 -0500

    convert string to numeric for total possible; add first/last names for total possible row.

--- R/gradebook.R
+++ R/gradebook.R
@@ -13,9 +13,11 @@ ww_read_gradebook <- function(path) {
   gradebook <-
     read.csv(path, skip = 7, header = FALSE, stringsAsFactors = FALSE)
   gradebook <-
-    rbind(gradebook, raw[6,])
+    rbind(gradebook, as.numeric(raw[6,]))
   names(gradebook) <- ifelse (nchar(raw[2, ]) > 0, raw[2, ], names(gradebook))
   names(gradebook)[1:6] <- c("student_id", "login_id", "last", "first", "section", "recitation")
   names(gradebook) <- gsub("%", "pct_", names(gradebook))
+  gradebook[nrow(gradebook), "first"] <- "Total"
+  gradebook[nrow(gradebook), "last"] <- "Possible"
   gradebook
 }
