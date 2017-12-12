dd84a7060a2ac54b91bdd45b047f2191a80c393a
Randall Pruim <rpruim@gmail.com>
2017-12-12 09:41:55 -0500

    add gradebook ingestion function

---
+++ R/gradebook.R
@@ -0,0 +1,21 @@
+
+#' Ingest WeBWorK gradebook
+#'
+#' @param path Path to gradebook file exported from WeBWorK
+#' @export
+#' @importFrom utils read.csv
+
+ww_read_gradebook <- function(path) {
+  raw <-
+    as.matrix(
+      read.csv(path, header = FALSE, stringsAsFactors = FALSE)
+    )
+  gradebook <-
+    read.csv(path, skip = 7, header = FALSE, stringsAsFactors = FALSE)
+  gradebook <-
+    rbind(gradebook, raw[6,])
+  names(gradebook) <- ifelse (nchar(raw[2, ]) > 0, raw[2, ], names(gradebook))
+  names(gradebook)[1:6] <- c("student_id", "login_id", "last", "first", "section", "recitation")
+  names(gradebook) <- gsub("%", "pct_", names(gradebook))
+  gradebook
+}
